a27b91eaa1c165063e7c9ad579d3878b56eb5007
soup-works <soupwork@users.noreply.github.com>
2022-01-28 20:46:15 +1100

    Add files via upload

--- exchangeratesAPI.py
+++ exchangeratesAPI.py
@@ -20,63 +20,87 @@ import time
 
 #sample url http://api.currencylayer.com/historical?access_key=blahblahblah&date=2010-04-18&currencies=USD,AUD,CAD,PLN,MXN&format=1c
 #
-# sample run command python exchangeratesAPI.py -d '2021-07-01' -j 'AUDUSD-2021jul01.json'
-# APIKEY=input("paste in the api key here. i won't save it. i promise.")
+
+def callAPI(sourcelist, sourcecurrency, destcurrency):
+    #sourcelist is a list of dates in text format
+    jsonlist = [] #init json list
+    #lazylazylazy
+    #APIKEY='testtesttesttestblahblahblah'
+    #muchmorebetter
+    APIKEY=getpass.getpass(prompt="paste the api key here (hidden)")
+    URLBASE='http://api.currencylayer.com/historical'
+    for date in sourcelist:
+        #
+        url=f'{URLBASE}?access_key={APIKEY}&date={date}&currencies={sourcecurrency},{destcurrency}&format=1c'
+        convert=requests.get(url)
+        jsonlist.append(convert.json())
+        time.sleep(1) #pause 1 second
+
+    return(jsonlist)
+    #end callAPI
+
+
+# sample run command python exchangeratesAPI.py -d '2021-07-01' -j 'AUDUSD-2021jul01'
+
 print("Exchange Rates via 'api.currencylayer.com'")
-#lazylazylazy
-#APIKEY='blahblahblahblahblah'
-#muchmorebetter
-APIKEY=getpass.getpass(prompt="paste the api key here (hidden)")
-URLBASE='http://api.currencylayer.com/historical'
 #
 sourcepath='d:/dougsprogs/exchangerates/'
 jsonfile=''
-csvfile='testdays.csv'
-dt_source=date.today()
-sourcecurrency='AUD'
-destcurrency='USD'
-testdate='2020-01-07'
+csvfile=''
+today = date.today()
+sourcecurrency=''
+destcurrency=''
 sourcelist = []
-
 jsonlist=[]
-inputargs=argparse.ArgumentParser()
+
+inputargs=argparse.ArgumentParser(description='This is a short program to hit an api \
+    (/api.currencylayer.com) , pull conversions on dates from a csv \
+    and save the output in json format. ',
+    epilog='the input csv should be a list of dates YYYY-MM-DD.\
+    The program will need an api key, which can be obtained from api.currencylayer.com .')
+
 inputargs.add_argument('-c', help='conversion request, source-destination, default AUD-USD')
-inputargs.add_argument('-d', help='date in  YYYY-MM-DD')
-inputargs.add_argument('-l', help='csv file with a list of dates, YYYY-MM-DD')
+inputargs.add_argument('-d', help='date in  YYYY-MM-DD , for a single day conversion')
+inputargs.add_argument('-l', help='csv file for input with a list of dates, YYYY-MM-DD')
 inputargs.add_argument('-j', help='json file to save output')
-#inputargs.add_argument('-csv', help='csv file date,sourceamt,source currency,dest currency')
+inputargs.add_argument('-p', help='file path for both input and output files')
 
 cliargs = inputargs.parse_args()
 currencies = cliargs.c
 sourcedate = cliargs.d
 sourcelist = cliargs.l 
-jsonfile = cliargs.j
+
+if cliargs.p:
+    sourcepath = cliargs.p
+if cliargs.j:
+    jsonfile = cliargs.j
 
 if currencies:
-    sourcecurrency,destcurrency = currencies.split('-')   
+    sourcecurrency,destcurrency = currencies.split('-')  
+else:
+    sourcecurrency = 'AUD' 
+    destcurrency = 'USD'   
 
-if sourcedate:
-    sourcelist = [sourcedate]
-
-elif sourcelist and not sourcedate:
-    csvfile = sourcelist
-
-#no need to open a file for a single date
-if not sourcedate:
-    with open((sourcepath + csvfile), 'r' ) as sourcefile:
+# a csv file provided on command line
+if not sourcedate and sourcelist:
+    with open((sourcepath + sourcelist), 'r' ) as sourcefile:
         sourcein = sourcefile.read()
         #sourcein is now a string spanning multiple lines
+        #repurpose sourcelist to be the list of dates
         sourcelist = sourcein.split()
 
-for checkdate in sourcelist:
-    #
-    url=f'{URLBASE}?access_key={APIKEY}&date={checkdate}&currencies={sourcecurrency},{destcurrency}&format=1c'
-    print ("url = ", url)
-    convert=requests.get(url)
-    jsonlist.append(convert.json())
-    time.sleep(1) #pause 1 second
-    print (f"json convert is {convert}")
-    print(f"json list is {jsonlist}")
+elif sourcedate:
+    #create a one-element list
+    sourcelist = [str(sourcedate)]
+
+else: #no csv or date provided
+    sourcelist=[str(today)]    
+    print("no csv or date provided")
+    print(f"source list is {sourcelist}")
+    print(f"source list type is {type(sourcelist)}")
+
+#Call the API
+jsonlist = callAPI(sourcelist, sourcecurrency, destcurrency)
 
 if jsonfile:
     print(f"writing json file {jsonfile}")
